b2fe49485064a88a6f3cc2019fc806a53c4d8ca5
fabianarequena <fabianarequena@usf.edu>
2025-02-09 10:56:40 -0500

    Task 2 - Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -10,3 +10,17 @@ calculateProfit(50, 70, 200);
 
 // Created formula to calculate profit given 3 parameters
 
+
+// Task 2: Sales Tax Computation
+
+function calculateSalesTax(amount, taxRate) {
+    let tax = amount * taxRate;
+    console.log(`Sales Tax: $${tax}`);
+
+};
+
+calculateSalesTax(100, 0.07); // Expected output: "Sales Tax: $7"
+calculateSalesTax(500, 0.1);  // Expected output: "Sales Tax: $50"
+
+// Created a function with two parameters to calculate salex tax
+
