9b66553fe898db07dd2505aa6218948838f4cd61
fabianarequena <fabianarequena@usf.edu>
2025-02-09 10:48:10 -0500

    Task 1 - Business Profit Calculation

---
+++ cc_6.js
@@ -0,0 +1,12 @@
+// Task 1: Business Profit Calculation
+
+function calculateProfit(costPrice, sellingPrice, unitsSold) {
+    let profit = (sellingPrice - costPrice) * unitsSold;
+    console.log(`Total Profit: $${profit}`);
+}
+
+calculateProfit(20, 30, 100);
+calculateProfit(50, 70, 200);
+
+// Created formula to calculate profit given 3 parameters
+
