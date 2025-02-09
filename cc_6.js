7fe75d16ae67c2ee7d6c69400620a571a2ef898f
fabianarequena <fabianarequena@usf.edu>
2025-02-09 11:21:58 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -24,3 +24,25 @@ calculateSalesTax(500, 0.1);  // Expected output: "Sales Tax: $50"
 
 // Created a function with two parameters to calculate salex tax
 
+
+// Task 3: Employee Bonus Calculation
+
+const calculateBonus = (salary, performanceRating) => {
+    let bonus = 0;
+
+    if (performanceRating === "Excellent") {
+        bonus = salary * 0.2;
+    } else if (performanceRating === "Good") {
+        bonus = salary * 0.1;
+    } else if (performanceRating === "Average") {
+        bonus = salary * 0.05;
+    }
+
+    console.log(`Bonus: $${bonus}`);
+};
+
+calculateBonus(5000, "Excellent"); // Expected output: "Bonus: $1000"
+calculateBonus(7000, "Good");      // Expected output: "Bonus: $700"
+
+// Calculated bonus based on performance ratings and percentages
+
